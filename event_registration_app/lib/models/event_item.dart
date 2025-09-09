afe1a32db0093aed99b09f70214adf156d87867d
Joyce Silva <joycepereirasbs@gmail.com>
2025-09-08 22:10:29 -0300

    feature: initialized the project and added a basic structure

---
+++ event_registration_app/lib/models/event_item.dart
@@ -0,0 +1,61 @@
+class EventRegistrationItem {
+  static int _nextId = 1;
+  final int id;
+  String date;
+  String time;
+  String eventName;
+  String eventDescription;
+
+  EventRegistrationItem({
+    required this.date,
+    required this.time,
+    required this.eventName,
+    required this.eventDescription,
+  }) : id = _nextId++;
+
+  EventRegistrationItem._withExistingId({
+    required this.id,
+    required this.date,
+    required this.time,
+    required this.eventName,
+    required this.eventDescription,
+  });
+
+  factory EventRegistrationItem.fromJson(Map<String, dynamic> json) {
+    final int incomingId = json['id'];
+    if (incomingId >= _nextId) {
+      _nextId = incomingId + 1;
+    }
+    return EventRegistrationItem._withExistingId(
+      id: incomingId,
+      date: json['date'],
+      time: json['time'],
+      eventName: json['eventName'],
+      eventDescription: json['eventDescription'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'date': date,
+      'time': time,
+      'eventName': eventName,
+      'eventDescription': eventDescription,
+    };
+  }
+  EventRegistrationItem copyWith({
+    String? date,
+    String? time,
+    String? eventName,
+    String? eventDescription,
+  }) {
+    return EventRegistrationItem._withExistingId(
+      id: this.id,
+      date: date ?? this.date,
+      time: time ?? this.time,
+      eventName: eventName ?? this.eventName,
+      eventDescription: eventDescription ?? this.eventDescription,
+    );
+  }
+}
---
+++ event_registration_app/lib/repositories/event_repository.dart
@@ -0,0 +1,48 @@
+import 'package:event_registration_app/models/event_item.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+import 'package:event_registration_app/utils/log.dart';
+
+class EventRepository {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/events.json');
+  }
+
+  Future<List<EventRegistrationItem> getAllEvents() async {
+    try {
+      final file = await _localFile;
+      final contents = await file.readAsString();
+      if (! await file.exists() || contents.isEmpty) {
+        return [];
+      }
+      final List<dynamic> jsonData = json.decode(contents);
+      return jsonData
+          .map((item) => EventRegistrationItem.fromJson(item))
+          .toList();
+    } catch (e, s) {
+      Log.e("Erro ao ler o arquivo", error: e, stackTrace: s);
+      return [];
+    }
+  }
+
+  Future<void> removeEvent(int id) async {
+    try {
+      final events = await getAllEvents();
+      events.removeWhere((event) => event.id == id);
+      final file = await _localFile;
+      final jsonData = events.map((e) => e.toJson()).toList();
+      await file.writeAsString(json.encode(jsonData));
+    } catch (e, s) {
+      Log.e("Erro ao remover o evento", error: e, stackTrace: s);
+    }
+  }
+
+
+}
